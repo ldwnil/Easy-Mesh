c659627d8e9a3b8b1992263143c67604cbd087cc
ldwnil <danial7980@gmail.com>
2026-05-29 11:42:01 +0330

    Update Core

--- README.md
+++ README.md
@@ -7,26 +7,9 @@ A simple script to set up mesh network based on easytier core
 
 Run the following script on your server (Debian or Ubuntu):
 
-v1
-```
-bash <(curl -Ls --ipv4 https://github.com/Musixal/easy-mesh/raw/main/easymesh.sh)
-```
 
-v2
-```
-bash <(curl -Ls --ipv4 https://github.com/Musixal/easy-mesh/raw/main/easymesh_v2.sh)
-```
-## My Telegram channel
-Check the channel below for more information:
-https://t.me/Gozar_Xray
+bash <(curl -Ls --ipv4 https://github.com/ldwnil/easy-mesh/raw/main/easymesh.sh)
 
- ## Support the project
-
-Donate TRX (TRC-20) to support our project:
-``` wallet
-TMVBGzX4qpt12R1qWsJMpT1ttoKH1kus1H
-```
-Thanks for your support! 
 
 ## Source code
 https://github.com/EasyTier/EasyTier
--- easymesh.sh
+++ easymesh.sh
@@ -78,9 +78,9 @@ install_easytier() {
     #URL_ARM_HARD="https://github.com/EasyTier/EasyTier/releases/download/v1.1.0/easytier-armv7-unknown-linux-musleabihf-v1.1.0.zip"
 
     #New Version
-    URL_X86="https://github.com/Musixal/Easy-Mesh/raw/main/core/v1.2.0/easytier-linux-x86_64/"
-    URL_ARM_SOFT="https://github.com/Musixal/Easy-Mesh/raw/main/core/v1.2.0/easytier-linux-armv7/"              
-    URL_ARM_HARD="https://github.com/Musixal/Easy-Mesh/raw/main/core/v1.2.0/easytier-linux-armv7hf/"
+    URL_X86="https://github.com//ldwnil/Easy-Mesh/tree/main/core/v2.6.4/easytier-linux-x86_64/"
+    URL_ARM_SOFT="https://github.com/ldwnil/Easy-Mesh/tree/main/core/v2.6.4/easytier-linux-armv7/"              
+    URL_ARM_HARD="https://github.com/ldwnil/Easy-Mesh/tree/main/core/v2.6.4/easytier-linux-armv7hf/"
     
     # Check if the directory exists
     if [ -d "$DEST_DIR" ]; then    
@@ -726,9 +726,8 @@ echo -e "   ${CYAN}╔═══════════════════�
 echo -e "   ║            🌐 ${WHITE}EasyMesh                 ${CYAN}║"
 echo -e "   ║        ${WHITE}VPN Network Solution            ${CYAN}║"
 echo -e "   ╠════════════════════════════════════════╣"
-echo -e "   ║  ${WHITE}Version: 0.97 beta                    ${CYAN}║"
-echo -e "   ║  ${WHITE}Telegram Channel: @Gozar_Xray         ${CYAN}║"
-echo -e "   ║  ${WHITE}GitHub: github.com/Musixal/easy-mesh  ${CYAN}║"
+echo -e "   ║  ${WHITE}Core Version: 2.6.4                   ${CYAN}║"
+echo -e "   ║  ${WHITE}GitHub: github.com/ldwnil/easy-mesh  ${CYAN}║"
 echo -e "   ╠════════════════════════════════════════╣${RESET}"
 echo -e "   ║        $(check_core_status)         ║"
 echo -e "   ╚════════════════════════════════════════╝"
