0c0731836e106dd5da1106766f11c8fef4615811
ldwnil <danial7980@gmail.com>
2026-05-29 12:07:15 +0330

    Update easymesh.sh

--- easymesh.sh
+++ easymesh.sh
@@ -723,11 +723,11 @@ display_menu() {
     clear
 # Print the header with colors
 echo -e "   ${CYAN}╔════════════════════════════════════════╗"
-echo -e "   ║            🌐 ${WHITE}EasyMesh                  ${CYAN}║"
+echo -e "   ║            🌐 ${WHITE}EasyMesh                   ${CYAN}║"
 echo -e "   ║        ${WHITE}VPN Network Solution            ${CYAN}║"
 echo -e "   ╠════════════════════════════════════════╣"
 echo -e "   ║  ${WHITE}Core Version: 2.6.4                   ${CYAN}║"
-echo -e "   ║  ${WHITE}GitHub: github.com/ldwnil/easy-mesh   ${CYAN}║"
+echo -e "   ║  ${WHITE}GitHub: github.com/ldwnil/easy-mesh    ${CYAN}║"
 echo -e "   ╠════════════════════════════════════════╣${RESET}"
 echo -e "   ║        $(check_core_status)         ║"
 echo -e "   ╚════════════════════════════════════════╝"
